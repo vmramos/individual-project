2fa7a25d3eda931eaaacd3e4aac87deb4c0629f4
Veronica <vm.ramos16@gmail.com>
2018-11-29 00:43:03 -0600

    Add files via upload

---
+++ individual-project/assets/js/local-calls.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function() {
+ 
+// Your code goes here...
+ 
+});
+
+
+2
+3
+$('.gallery').cycle({
+		fx: 'curtainX' // choose your transition type, ex: fade, scrollUp, shuffle, etc...
+	});
+	
